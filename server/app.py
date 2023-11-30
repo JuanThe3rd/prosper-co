3eabddfa977d9abefef73efbb81c8378bf4c7d74
Juan Barajas <juan.barajas4406@my.riohondo.edu>
2023-11-30 11:27:21 -0800

    creates server, yet to be tested

--- server/app.py
+++ server/app.py
@@ -1,22 +1,146 @@
 #!/usr/bin/env python3
-
-# Standard library imports
-
-# Remote library imports
-from flask import request
+from flask import request, make_response
 from flask_restful import Resource
+import os
 
-# Local imports
 from config import app, db, api
-# Add your model imports
+from models import Account, Product, Order
 
-
-# Views go here!
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+DATABASE = os.environ.get(
+    "DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
 
 @app.route('/')
 def index():
     return '<h1>Phase 4 Project Server</h1>'
 
+# Views go here!
+class Accounts(Resource):
+    def get(self, id = None):
+        if id:
+            account = Account.query.filter_by(id = id).first()
+
+            if account:
+                return make_response(account.to_dict(), 200)
+            else:
+                return make_response({'error': 'Account not found'}, 404)
+        else:
+            accounts = Account.query.all()
+            return make_response([account.to_dict() for account in accounts], 200)
+
+    def post(self):
+        new_account = Account(**request.json)
+        db.session.add(new_account)
+        db.session.commit()
+        return make_response(new_account.to_dict(), 201)
+
+    def patch(self, id):
+        account = Account.query.filter_by(id = id).first()
+
+        if account:
+            for key, value in request.json.items():
+                setattr(account, key, value)
+                db.session.commit()
+                return make_response(account.to_dict(), 200)
+        else:
+            return make_response({'error': 'Account not found'})
+        
+    def delete(self, id):
+        account = Account.query.filter_by(id = id).first()
+
+        if account:
+            db.session.delete(account)
+            db.session.commit()
+            return make_response({'result': 'Account deleted'}, 200)
+        else:
+            return make_response({'error': 'Account not found'}, 404)
+
+
+class Products(Resource):
+    def get(self, id = None):
+        if id:
+            product = Product.query.filter_by(id = id).first()
+
+            if product:
+                return make_response(product.to_dict(), 200)
+            else:
+                return make_response({'error': 'Product not found'}, 404)
+        else:
+            products = Product.query.all()
+            return make_response([product.to_dict() for product in products], 200)
+
+    def post(self):
+        new_product = Product(**request.json)
+        db.session.add(new_product)
+        db.session.commit()
+        return make_response(new_product.to_dict(), 201)
+
+    def patch(self, id):
+        product = Product.query.filter_by(id = id).first()
+
+        if product:
+            for key, value in request.json.items():
+                setattr(product, key, value)
+                db.session.commit()
+                return make_response(product.to_dict(), 200)
+        else:
+            return make_response({'error': 'Product not found'})
+
+    def delete(self, id):
+        product = Product.query.filter_by(id = id).first()
+
+        if product:
+            db.session.delete(product)
+            db.session.commit()
+            return make_response({'result': 'Product deleted'}, 200)
+        else:
+            return make_response({'error': 'Product not found'}, 404)
+
+
+class Orders(Resource):
+    def get(self, id = None):
+        if id:
+            order = Order.query.filter_by(id = id).first()
+
+            if order:
+                return make_response(order.to_dict(), 200)
+            else:
+                return make_response({'error': 'Order not found'}, 404)
+        else:
+            orders = Order.query.all()
+            return make_response([order.to_dict() for order in orders], 200)
+
+    def post(self):
+        new_order = Order(**request.json)
+        db.session.add(new_order)
+        db.session.commit()
+        return make_response(new_order.to_dict(), 201)
+
+    def patch(self, id):
+        order = Order.query.filter_by(id = id).first()
+
+        if order:
+            for key, value in request.json.items():
+                setattr(order, key, value)
+                db.session.commit()
+                return make_response(order.to_dict(), 200)
+        else:
+            return make_response({'error': 'Order not found'})
+        
+    def delete(self, id):
+        order = Order.query.filter_by(id = id).first()
+
+        if order:
+            db.session.delete(order)
+            db.session.commit()
+            return make_response({'result': 'Order deleted'}, 200)
+        else:
+            return make_response({'error': 'Order not found'}, 404)
+
+
+api.add_resource(Accounts, '/accounts', '/accounts/<int:id>')
+api.add_resource(Products, '/products', '/products/<int:id>')
+api.add_resource(Orders, '/orders', '/orders/<int:id>')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -4,3 +4,46 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+class Account(db.Model, SerializerMixin):
+    __tablename__ = 'accounts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    firstname = db.Column(db.String, nullable=False)
+    lastname = db.Column(db.String, nullable=False)
+    address = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<Account Name: {self.firstname} {self.lastname} />'
+
+
+class Product(db.Model, SerializerMixin):
+    __tablename__ = 'products'
+
+    id = db.Column(db.Integer, primary_key=True)
+    category = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.String)
+    price = db.Column(db.Integer, nullable=False)
+    sizes = db.Column(db.String, nullable=False)
+
+    serialize_rules = ('-order.products')
+
+    def __repr__(self):
+        return f'<Product {self.name} />'
+
+
+class Order(db.Model, SerializerMixin):
+    __tablename__ = 'orders'
+
+    id = db.Column(db.Integer, primary_key=True)
+    account_id = db.Column(db.Integer, db.ForeignKey('products.id'))
+    total = db.Column(db.Float, nullable=False)
+
+    products = db.relationship('Product', cascade = 'all, delete', backref='order')
+
+    serialize_rules = ('-products.order',)
+
+    def __repr__(self):
+        return f'<Order To: {self.account.firstname} {self.account.lastname} Total: {self.total}'
