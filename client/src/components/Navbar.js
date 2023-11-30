80551084dff126f68aa75fc2109129fc70a64f4b
Juan Barajas <juan.barajas4406@my.riohondo.edu>
2023-11-30 10:27:38 -0800

    home page layout complete

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 function Navbar() {
     return (
-        <div className='navbar-container'>
-            <a className='nav-link' href='/'>Home</a>
-            <a className='nav-link'>About Us</a>
-            <a className='nav-link'>Cart</a>
-            <a className='nav-link'>Sign-In</a>
+        <div>
+            <div className='navbar-container'>
+                <img className='nav-home-logo' src={require('../site-images/prosper-logo.png')} title='Home' alt='home-logo' />
+                <a className='nav-link'>About Us</a>
+                <a className='nav-link'>Cart</a>
+                <a className='nav-link'>Sign-In</a>
+            </div>
         </div>
     )
 }
