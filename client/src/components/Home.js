a22f6eff1ec33ed89d9fb2e14a1c4457d6561f60
Juan Barajas <juan.barajas4406@my.riohondo.edu>
2023-12-02 14:48:28 -0800

    adds css and basic cart functionality, needs more css

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,19 +3,18 @@ import Navbar from './Navbar';
 
 function Home() {
     const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState([]);
 
     useEffect(() => {
         fetch('/products')
             .then(res => res.json())
             .then(setProducts)
-    }, [])
-
-    console.log(products);
+    }, []);
 
     return (
         <div>
             <div className='page-background'></div>
-            <Navbar />
+            <Navbar cart={cart}/>
             <div className='home-page'>
                 <h1 className='home-title'>Prosper Co.</h1>
 
@@ -23,7 +22,14 @@ function Home() {
                     {products.map((product) => (
                         <div className='product-card'>
                             <img className='product-img' src={product.picture} alt={`${product.name} img`} />
-                            <h2>{product.name}</h2>
+                            <p className='product-title'>{product.name}</p>
+                            <div className='product-details'>
+                                <p className='product-price'>Price: ${product.price}</p>
+                                <button className='add-to-cart-btn' onClick={() => addProductToCart(product)}>Add to Cart</button>
+                                {product.description &&
+                                    <p>{product.description}</p>
+                                }
+                            </div>
                         </div>
                     ))}
                 </div>
@@ -35,6 +41,10 @@ function Home() {
             </div>
         </div>
     )
+
+    function addProductToCart(product){
+        setCart([...cart, product])
+    }
 }
 
 export default Home;
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,16 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Navbar() {
+function Navbar({ cart }) {
+    const [modal, setModal] = useState(null);
+
+    console.log(cart);
+        
     return (
         <div>
             <div className='navbar-container'>
                 <img className='nav-home-logo' src={require('../site-images/prosper-logo.png')} title='Home' alt='home-logo' />
                 <a className='nav-link' href="">About Us</a>
-                <a className='nav-link' href="">Cart</a>
+                <a className='nav-link' onClick={handleCartClick}>Cart</a>
                 <a className='nav-link' href="">Sign-In</a>
             </div>
+
+            {modal &&
+                <div className='modal-container'>
+                    <span className='close-modal' onClick={() => setModal(null)}>&times;</span>
+                    <h2 className='modal-title'>Cart</h2>
+                    <div className='cart-items-container'>
+                        {cart.length !== 0 &&
+                            <div>
+                                {cart.map((product) => (
+                                    <div>
+                                        <h3>{product.name}</h3>
+                                    </div>
+                                ))}
+                            </div>
+                        }
+                        {cart.length === 0 &&
+                            <div>
+                                <h3 className='empty-cart'>Your Cart is Empty</h3>
+                            </div>
+                        }
+                    </div>
+
+                    <div className='modal-checkout-details'>
+                        <p className='modal-total'>Total: $0</p>
+                        <button className='modal-checkout-btn'>Checkout</button>
+                    </div>
+                </div>
+            }
         </div>
     )
+
+    function handleCartClick(e){
+        if (modal === null){
+            setModal(true);
+        } else if (modal !== null){
+            setModal(null);
+        }
+    }
 }
 
 export default Navbar;
